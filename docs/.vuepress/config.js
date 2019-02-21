229379028a25f3ad2772b07bf80c48a9be050f2f
fys <723107234@qq.com>
2019-02-21 22:17:26 +0800

    travis-ci配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,12 +2,16 @@ module.exports = {
     title: 'leetcode-javascript-answer',
     description: 'leetcode中使用javascript的解题答案',
     base: '/leetcode-javascript-answer/',
-    dest: 'docs',
     head: [
     ],
     contentLoading: true,
     markdown: {
         lineNumbers: true,
         toc: { includeLevel: [1, 2, 3] }
+    },
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+        ]
     }
 }
