1153cf6ea997f5a2f3f27b2856b4ac99a480e25d
fys <723107234@qq.com>
2019-02-21 21:58:37 +0800

    add code

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: 'leetcode-javascript-answer',
+    description: 'leetcode中使用javascript的解题答案',
+    base: '/leetcode-javascript-answer/',
+    dest: 'docs',
+    head: [
+    ],
+    contentLoading: true,
+    markdown: {
+        lineNumbers: true,
+        toc: { includeLevel: [1, 2, 3] }
+    }
+}
