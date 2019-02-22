c35e28ae420d2f05eb8cafeb8e49f9f9f444318f
fanyushun <723107234@qq.com>
2019-02-22 13:13:38 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'leetcode中使用javascript的解题答案',
     base: '/leetcode-javascript-answer/',
     head: [
+        ['link', { rel: 'icon', href: '/favicon.ico' }]
     ],
     contentLoading: true,
     markdown: {
